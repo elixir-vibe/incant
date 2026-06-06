c2903e9ce84f8569b5e4cf1a9dc36cfe8a61a2d9
Danila Poyarkov <dev@dannote.net>
2026-06-06 09:53:27 +0300

    Use AST checks in installer patcher

--- lib/incant/install/patcher.ex
+++ lib/incant/install/patcher.ex
@@ -2,8 +2,7 @@ defmodule Incant.Install.Patcher do
   @moduledoc false
 
   def ensure_router_import(content) do
-    if String.contains?(content, "use Incant.Router") or
-         String.contains?(content, "import Incant.Router") do
+    if router_imported?(content) do
       content
     else
       String.replace(content, ~r/(use\s+[^\n]+,\s*:router\n)/, "\\1  use Incant.Router\n",
@@ -16,7 +15,7 @@ defmodule Incant.Install.Patcher do
     route = "incant_admin \"/admin\", #{namespace}.Admin"
 
     cond do
-      String.contains?(content, "incant_admin") ->
+      incant_admin_route?(content) ->
         content
 
       String.contains?(content, "pipe_through :browser") ->
@@ -31,6 +30,38 @@ defmodule Incant.Install.Patcher do
         content <> "\n# Add inside your browser scope:\n# #{route}\n"
     end
   end
+
+  defp router_imported?(content) do
+    content
+    |> parse_router()
+    |> ast_contains?(fn
+      {:use, _meta, [{:__aliases__, _alias_meta, [:Incant, :Router]}]} -> true
+      {:import, _meta, [{:__aliases__, _alias_meta, [:Incant, :Router]}]} -> true
+      _node -> false
+    end)
+  end
+
+  defp incant_admin_route?(content) do
+    content
+    |> parse_router()
+    |> ast_contains?(fn
+      {:incant_admin, _meta, _args} -> true
+      _node -> false
+    end)
+  end
+
+  defp parse_router(content), do: Sourceror.parse_string(content)
+
+  defp ast_contains?({:ok, ast}, predicate) do
+    {_ast, found?} =
+      Macro.prewalk(ast, false, fn node, found? ->
+        {node, found? or predicate.(node)}
+      end)
+
+    found?
+  end
+
+  defp ast_contains?(_error, _predicate), do: false
 
   def ensure_incant_source(content) do
     if String.contains?(content, "@source \"../deps/incant/lib\"") do
